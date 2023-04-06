e3478a41e3183b050b46b814e423729aa8fbf3f1
Kacperuss <kacperradziminski@wp.pl>
2023-04-06 14:23:11 +0200

    Checking answers

--- nuxt2/components/sections/MainGuess.vue
+++ nuxt2/components/sections/MainGuess.vue
@@ -15,34 +15,45 @@
                 <img v-if="image" :src="image" class="block w-full h-full absolute top-0 left-0" />
             </div>
             <div id="answer_form" v-if="!answered">
-                <button class="_skip" @click="show_answer()">Show answer</button>
-                <!-- <button class="_skip" @click="show_answer()">Give up</button> -->
-                <!-- <input v-if="loading" type="disabled" disabled="disabled" /> -->
-                <!-- <input v-else type="text" @keypress="on_key_input" /> -->
-                <!-- <button class="_enter" @click="check_answer()">Enter</button> -->
+                <!-- <button class="_skip" @click="show_answer()">Show answer</button> -->
+                <button class="_skip" @click="show_answer()">Give up</button>
+                <input v-if="loading" type="disabled" disabled="disabled" />
+                <input v-else type="text" @keypress="on_key_input" />
+                <button class="_enter" @click="check_answer()">Enter</button>
             </div>
             <div id="answer_result" v-if="answered" class="">
                 <div class="w-25"></div>
-                <div class="_game_name">{{ names[0] }}</div>
+                <div class="_game_name" :class="correct ? 'correct' : 'skipped'">{{ names[0] }}</div>
                 <button class="_next" @click="load_next()">Next</button>
             </div>
             <div class="">
-                <button class="_report w-70" @click="show_report()">Mark / report the screenshot</button>
+                <!-- <button class="_report w-70" @click="show_report()">Mark / report the screenshot</button> -->
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import levenshtein from 'js-levenshtein'
+
+const $ = (x) => document.querySelector(x)
+const $$ = (x) => document.querySelectorAll(x)
+
 export default {
     data: () => ({
         answered: false,
+        correct: false,
+        can_go_next: false,
     }),
     props: ['image', 'names', 'loading'],
     methods: {
-        show_answer() {
+        show_answer(correct = false) {
             if (this.loading) return
+            this.correct = correct
             this.answered = true
+            setTimeout(() => {
+                this.can_go_next = true
+            }, 50)
         },
         on_key_input(e) {
             if (this.loading) return
@@ -54,21 +65,40 @@ export default {
         load_next() {
             if (this.loading) return
             this.answered = false
+            this.correct = false
+            this.can_go_next = false
             this.$parent.loading = true
             setTimeout(() => {
                 this.$parent.set_random_game()
             }, 50)
         },
+        normalise_name(name) {
+            return name.toLowerCase().replaceAll(/[^a-z]/g, '')
+        },
         check_answer() {
             if (this.loading) return
-            console.log('check')
+            // console.log('check')
+            const input = $('#answer_form input')
+            if (!input || !input.value) return
+            let guess = input.value.toLowerCase()
+            let norm_guess = this.normalise_name(guess) || guess
+            for (let i = 0; i < this.names.length; ++i) {
+                let x = this.names[i].toLowerCase()
+
+                if (x.length > 6 && x.search(guess) != -1 && x.length < guess.length * 2) return this.show_answer(true)
+
+                let xx = this.normalise_name(x) || x
+                let err = Math.min(levenshtein(x, guess) / x.length, levenshtein(xx, norm_guess) / xx.length)
+                if (err < 0.26) return this.show_answer(true)
+            }
         },
         show_report() {
             console.log('report')
         },
 
         on_keypress_body(e) {
-            const input = document.querySelector('#answer_form input')
+            const input = $('#answer_form input')
+            if (!input && this.answered && this.can_go_next) return this.load_next()
             if (!input) return
             if (e.target == input) return
             input.focus()
@@ -153,6 +183,12 @@ button {
     ._game_name {
         font-size: calc(32rem / 16);
         line-height: 1;
+        &.correct {
+            color: rgb(98, 173, 79);
+        }
+        &.skipped {
+            color: rgb(216, 190, 77);
+        }
     }
     ._next {
         background: rgb(46, 117, 28);
